3953d0ce80e039f7e3c0375568a30663310dad4c
Josh Leath <joshua.leath@icloud.com>
2020-08-24 19:51:19 -0400

    completed mortgage calculator

--- lesson_2/mortgage_calculator/mortgage_calculator.rb
+++ lesson_2/mortgage_calculator/mortgage_calculator.rb
@@ -1,12 +1,15 @@
-def prompt(message)
-  puts ">> #{message}" 
+require 'yaml'
+MESSAGES = YAML.load_file('mortgage_calculator_messages.yml')
+
+def prompt(message, postfix='', lang='en')
+  puts ">> #{MESSAGES[lang][message]}#{postfix}"
 end
 
-def calculate_monthly_rate(loan_amount, monthly_rate, loan_duration_months)
+def calculate_rate(loan_amount, monthly_rate, loan_months)
   if monthly_rate == 0.0
-    loan_amount / loan_duration_months
+    loan_amount / loan_months
   else
-    loan_amount * (monthly_rate / (1 - (1 + monthly_rate)**(-loan_duration_months)))
+    loan_amount * (monthly_rate / (1 - (1 + monthly_rate)**-loan_months))
   end
 end
 
@@ -16,46 +19,60 @@ def get_input(message, valid_fn)
     prompt message
     input = gets.chomp
     break if valid_fn.call(input)
-    prompt "Sorry, there is something wrong with your answer. Please try again."
+
+    prompt 'invalid_input'
   end
   input
 end
 
 def get_yes_no(message)
-  valid_fn = lambda { |input| %w(y n Y N).include?(input) }
+  valid_fn = ->(input) { %w[y n Y N].include?(input) }
   get_input(message, valid_fn).downcase
 end
 
 def get_positive_int(message)
-  valid_fn = lambda { |input| /^\d+$/.match(input) }
-  result = get_input(message, valid_fn).to_i
+  valid_fn = ->(input) { /^\d+$/.match(input) }
+  result = nil
+  loop do
+    result = get_input(message, valid_fn).to_i
+    break if result.positive?
+
+    prompt 'non_positive_input'
+  end
+  result
 end
 
-def get_float(message)
-  valid_fn = lambda { |input| /^\d?.?\d+$/.match(input) }
-  get_input(message, valid_fn).to_f
+def get_positive_float(message)
+  valid_fn = ->(input) { /^\d?.?\d+$/.match(input) }
+  result = nil
+  loop do
+    result = get_input(message, valid_fn).to_f
+    break if result.positive?
+
+    prompt 'non_positive_input'
+  end
+  result
 end
 
 def get_percentage(message)
-  get_float(message) / 100.0
+  get_positive_float(message) / 100.0
 end
 
-prompt "Welcome to the Mortgage Calculator!"
+prompt 'welcome'
 
 loop do
-  loan_amount = get_float "Enter the amount of your loan in dollars (ex. 1234.34):"
-  annual_rate = get_percentage "Enter the annual percentage rate of the loan (ex. 5.0):"
+  loan_amount = get_positive_float 'enter_loan_amount'
+  annual_rate = get_percentage 'enter_apr'
   monthly_rate = annual_rate / 12.0
-  loan_duration_years = get_int "Enter the loan duration in years (ex 5):"
-  loan_duration_months = loan_duration_years * 12
-  monthly_payment = calculate_monthly_rate(loan_amount, monthly_rate, loan_duration_months)
+  loan_years = get_positive_int 'enter_duration'
+  loan_months = loan_years * 12
+  monthly_payment = calculate_rate(loan_amount, monthly_rate, loan_months)
 
-  prompt "Monthly Interest Rate: #{(monthly_rate * 100).round(2)}%"
-  prompt "Loan Duration (Months): #{loan_duration_months}"
-  prompt "Monthly Payment: $#{monthly_payment.round(2)}"
-  puts ""
+  prompt 'monthly_rate_output', "#{(monthly_rate * 100).round(2)}%"
+  prompt 'duration_output', loan_months.to_s
+  prompt 'payment_output', monthly_payment.round(2).to_s
 
-  break if get_yes_no("Would you like to run the Mortgage Calculator again?") == 'n'
+  break if get_yes_no('run_again') == 'n'
 end
 
-prompt "Thank you for using the Mortgage Calculator!"
+prompt 'goodbye'
