b04897667a08e657e8baf1829a9c7dd55baf6486
Josh Leath <lawnfurniture@icloud.com>
2020-08-26 13:14:32 -0400

    refactored original code

--- lesson_2/rps/rock_paper_scissors.rb
+++ lesson_2/rps/rock_paper_scissors.rb
@@ -1,46 +1,87 @@
-VALID_CHOICES = %w[rock paper scissors]
+require 'yaml'
+RPS_MESSAGES = YAML.load_file('rock_paper_scissors_messages.yml')
 
-def prompt(message)
-  puts "=> #{message}"
+SLEEP_TIME = 0.75
+VALID_SHAPES = %w[rock paper scissors]
+VALID_YES_NO = %w[yes no]
+
+WIN_CONDITIONS = {
+  rock: %w[scissors],
+  paper: %w[rock],
+  scissors: %w[paper]
+}
+
+def prompt(message, postfix='', lang='en')
+  prompt_msg = RPS_MESSAGES[lang]['prompt']
+  puts "#{prompt_msg} #{RPS_MESSAGES[lang][message]} #{postfix}"
+end
+
+def valid_selection?(choice)
+  VALID_SHAPES.include?(choice)
+end
+
+def get_input(message, valid_fn, postfix='')
+  loop do
+    prompt message, postfix
+    input = gets.chomp
+    return input if valid_fn.call(input)
+    prompt 'invalid_choice_msg'
+  end
+end
+
+def get_player_choice
+  valid_fn = ->(choice) { VALID_SHAPES.include?(choice) }
+  get_input('choose_shape_msg', valid_fn, VALID_SHAPES.join(', '))
+end
+
+def print_computer_choice_sequence
+  print RPS_MESSAGES['en']['prompt'] + ' '
+  print RPS_MESSAGES['en']['computer_thinking_msg']
+  3.times do
+    sleep(SLEEP_TIME)
+    print '.'
+  end
+  sleep(SLEEP_TIME)
+  puts ''
+end
+
+def get_computer_choice
+  print_computer_choice_sequence
+  choice = VALID_SHAPES.sample
+  prompt 'computer_chose_msg', choice
+  choice
 end
 
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  WIN_CONDITIONS[first.to_sym].include? second
 end
 
 def display_results(player, computer)
   if win?(player, computer)
-    prompt 'You won!'
+    prompt 'player_won_msg'
   elsif win?(computer, player)
-    prompt 'Computer won!'
+    prompt 'computer_won_msg'
   else
-    prompt "It's a tie!"
+    prompt 'tie_msg'
   end
 end
 
-choice = ''
+def play_again?
+  valid_fn = ->(input) { VALID_YES_NO.include?(input.downcase) }
+  answer = get_input('play_again_msg', valid_fn)
+  answer.downcase == 'yes'
+end
+
+def display_choices(player_choice, computer_choice)
+  prompt 'computer_chose_msg', computer_choice
+  prompt 'player_chose_msg', player_choice
+end
 
 loop do
-  loop do
-    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-    choice = gets.chomp
-
-    break if VALID_CHOICES.include?(choice)
-
-    prompt "That's not a valid choice."
-  end
-
-  computer_choice = VALID_CHOICES.sample
-
-  puts "You chose: #{choice}; Computer chose #{computer_choice}"
-
-  display_results(choice, computer_choice)
-
-  prompt 'Do you want to play again?'
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  player_choice = get_player_choice
+  computer_choice = get_computer_choice
+  display_results(player_choice, computer_choice)
+  break unless play_again?
 end
 
-prompt 'Thank you for playing. Good Bye!'
+prompt 'goodbye_msg'
